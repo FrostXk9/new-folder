0253d836e7d3121d6d051d8a4416b72151fd5c84
FrostXk9 <brandonroulstone465@gmail.com>
2023-06-15 15:03:49 +0200

    no comment

---
+++ script.js
@@ -0,0 +1,15 @@
+let age = 20;
+age = 65;
+
+if(age >=18) {
+    console.log("discount of 25%");
+} else {
+    console.log("there are no discounts available");
+}
+
+if(age >=65) {
+    console.log("discount of 50%");
+} else {
+    console.log("there are no discounts available");
+}
+
